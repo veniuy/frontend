e3ddac7a12233876582661832a8b881c7993a1bb
veniuy <nambiuruguay@gmail.com>
2025-09-09 23:21:16 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'   // <-- Importa tu AuthProvider
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>   {/* <-- Aquí envuelves toda tu app */}
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
