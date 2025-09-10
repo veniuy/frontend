a42e30ebe14d0c90d315f5dea3d6e60e4256fe73
veniuy <nambiuruguay@gmail.com>
2025-09-10 01:11:33 -0300

    Update Landing.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import { Button } from '../components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../components/ui/card'
 import { Badge } from '../components/ui/badge'
+import { asset, onImgError } from '../utils/assets';
 import { 
   Palette, 
   Users, 
