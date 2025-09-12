207efe8e95bad097683296ba33af1e129cc85472
veniuy <nambiuruguay@gmail.com>
2025-09-12 12:11:30 -0300

    Delete src directory

--- src/pages/categories/Bautizo.jsx
+++
@@ -1,9 +0,0 @@
-// Bautizo.jsx - Página de categoría para Bautizo / Baby Shower
-import React from 'react'
-import CategoryBase from './CategoryBase'
-
-const Bautizo = () => {
-  return <CategoryBase categorySlug="bautizo" />
-}
-
-export default Bautizo
