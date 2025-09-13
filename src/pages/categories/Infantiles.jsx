ace5d11e15d56672c6738f37c9d7621ba9efbc71
veniuy <nambiuruguay@gmail.com>
2025-09-13 16:10:06 -0300

    Delete src/pages directory

--- src/pages/categories/Infantiles.jsx
+++
@@ -1,9 +0,0 @@
-// Infantiles.jsx - Página de categoría para Cumpleaños Infantiles
-import React from 'react'
-import CategoryBase from './CategoryBase'
-
-const Infantiles = () => {
-  return <CategoryBase categorySlug="infantiles" />
-}
-
-export default Infantiles
