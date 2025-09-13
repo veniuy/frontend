078095d1d33d87a3f60095a4004d4f73336c3e6e
veniuy <nambiuruguay@gmail.com>
2025-09-12 22:29:10 -0300

    Delete src directory

--- src/pages/categories/Corporativos.jsx
+++
@@ -1,9 +0,0 @@
-// Corporativos.jsx - Página de categoría para Eventos Corporativos
-import React from 'react'
-import CategoryBase from './CategoryBase'
-
-const Corporativos = () => {
-  return <CategoryBase categorySlug="corporativos" />
-}
-
-export default Corporativos
