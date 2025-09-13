224d457f1b1a9589a73c0137fdff0266225fe17f
veniuy <nambiuruguay@gmail.com>
2025-09-13 16:42:05 -0300

    Update PageStylesPanel.jsx

--- src/components/editor/panels/PageStylesPanel.jsx
+++ src/components/editor/panels/PageStylesPanel.jsx
@@ -1,4 +1,4 @@
-import { Button } from '../ui/button'
+import { Button } from '../../ui/button'
 import { Card, CardContent } from '../ui/card'
 import { Badge } from '../ui/badge'
 import { FONT_FAMILIES } from '../../hooks/useInvitationEditor'
