53986c93e74b7a4eaec43c92c8a37e6f246b088d
veniuy <nambiuruguay@gmail.com>
2025-09-13 01:02:36 -0300

    Delete src directory

--- src/pages/categories/Graduaciones.jsx
+++
@@ -1,9 +0,0 @@
-// Graduaciones.jsx - Página de categoría para Graduaciones
-import React from 'react'
-import CategoryBase from './CategoryBase'
-
-const Graduaciones = () => {
-  return <CategoryBase categorySlug="graduaciones" />
-}
-
-export default Graduaciones
