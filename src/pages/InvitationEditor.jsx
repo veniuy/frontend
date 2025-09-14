2c711b14fb63f4527d47b80652f203efa03b5366
veniuy <nambiuruguay@gmail.com>
2025-09-14 20:05:02 -0300

    Delete src directory

--- src/pages/InvitationEditor.jsx
+++
@@ -1,7 +0,0 @@
-import WeddingInvitationEditor from '../components/WeddingInvitationEditor'
-
-const InvitationEditor = () => {
-  return <WeddingInvitationEditor />
-}
-
-export default InvitationEditor
