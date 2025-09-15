413024c7c08099007c3b56f9ac7de602f2f731cd
veniuy <nambiuruguay@gmail.com>
2025-09-14 21:31:36 -0300

    Add files via upload

---
+++ src/pages/categories/Boda.jsx
@@ -0,0 +1,9 @@
+// Boda.jsx - Página de categoría para Bodas
+import React from 'react'
+import CategoryBase from './CategoryBase'
+
+const Boda = () => {
+  return <CategoryBase categorySlug="boda" />
+}
+
+export default Boda
