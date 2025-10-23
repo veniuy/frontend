ce54b3b40e7a6d5af13e19341f5b9066a8307a2f
veniuy <nambiuruguay@gmail.com>
2025-10-22 23:00:44 -0300

    Update Products.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -7,6 +7,7 @@ import { Badge } from '../components/ui/badge'
 import { Input } from '../components/ui/input'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../components/ui/select'
 import PublicHeader from '../components/PublicHeader'
+import ImageScrollSection from './ImageScrollSection';
 import { 
   Search,
   Grid3X3,
@@ -166,6 +167,9 @@ function Products() {
 
   return (
     <div className="min-h-screen bg-background">
+      {/* Image Scroll Section */}
+      <ImageScrollSection />
+
       {/* Header */}
       <PublicHeader 
         cartCount={cartCount}
